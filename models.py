c3f5c70d6cee81d83a6600ef4e3df25fd918d76d
Cássio Filipe Meireles De Carvalho Braga <78364193+Cassio06@users.noreply.github.com>
2026-04-23 10:46:18 -0300

    Fase 3
    Criação das classes do models.py, definindo tamanho, tipo e condições de cada variavel de cada classe(Classes Task e User)

--- models.py
+++ models.py
@@ -1,1 +1,23 @@
- 
+
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from database import Base
+
+
+class User(Base):
+    __tablename__ = 'Users'
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(100), unique = True, nullable = False)
+    password = Column(String(30), unique = False, nullable = False)
+
+class Task(Base):
+    __tablename__ = 'Tasks'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(70), unique = True, nullable = False)
+    description = Column(String(200), unique = False, nullable = False)
+    completed = Column(Boolean, default = False, nullable = False)
+    owner_id = Column(Integer,ForeignKey("users.id"), unique = True, nullable = False)
+
+
